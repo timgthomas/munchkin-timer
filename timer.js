9116cf11cb08d764816079ba9cde833eca4a0cca
Tim G. Thomas <tim@timgthomas.com>
2014-09-13 17:24:21 -0500

    use a real egg timer

--- timer.js
+++ timer.js
@@ -5,6 +5,10 @@
 
   timer.addEventListener('transitionend', function() {
     timer.classList.remove('progress');
+    timer.classList.add('shake-constant');
+    setTimeout(function() {
+      timer.classList.remove('shake-constant');
+    }, 1500);
   });
 
   start.addEventListener('click', function() {
